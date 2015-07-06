4f868ba959116415523160900d977dbfd307e365
matrixzone <matrixzonee@gmail.com>
2015-07-06 21:55:47 +0530

    Update cm.mk

--- cm.mk
+++ cm.mk
@@ -1,10 +1,10 @@
 $(call inherit-product, device/samsung/d2att/full_d2att.mk)
 
 # Enhanced NFC
-$(call inherit-product, vendor/cm/config/nfc_enhanced.mk)
+$(call inherit-product, vendor/du/config/nfc_enhanced.mk)
 
 # Inherit some common CM stuff.
-$(call inherit-product, vendor/cm/config/common_full_phone.mk)
+$(call inherit-product, vendor/du/config/common_full_phone.mk)
 
 PRODUCT_BUILD_PROP_OVERRIDES += \
     PRODUCT_NAME=d2uc \
@@ -12,6 +12,6 @@ PRODUCT_BUILD_PROP_OVERRIDES += \
     BUILD_FINGERPRINT="samsung/d2uc/d2att:4.4.2/KOT49H/I747UCUFNJ1:user/release-keys" \
     PRIVATE_BUILD_DESC="d2uc-user 4.4.2 KOT49H I747UCUFNJ1 release-keys"
 
-PRODUCT_NAME := cm_d2att
+PRODUCT_NAME := du_d2att
 PRODUCT_DEVICE := d2att
 
